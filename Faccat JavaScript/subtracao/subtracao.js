dcd250c91e4ef12dbcdf6cc733d3c972c8b6fc64
Grazielly Raissa <147439694+GraziellyRaissa1@users.noreply.github.com>
2024-06-13 10:32:48 -0300

    Delete Faccat JavaScript directory

--- Faccat JavaScript/subtracao/subtracao.js
+++
@@ -1,4 +0,0 @@
-var numero1=parseInt(prompt("Digite um número"));
-var numero2= parseInt(prompt("Digite um outro numero"));
-resultado=numero1-numero2
-alert(" O resultado da subtracao é: "   +  resultado);
--- Faccat JavaScript/sucessor/sucessor.js
+++
@@ -1,3 +0,0 @@
-var numero1=parseInt(prompt("Digite um número"));
-resultado=numero1 + 1
-alert(" O sucessor de: "  +  numero1  +  " é  "  +  resultado);
